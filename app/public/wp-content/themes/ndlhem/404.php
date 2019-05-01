3d905548f83681c2a6b06c3aff08a82768cf47ba
Thomas <mr-sylar59@world-of-sylar.fr>
2019-05-01 13:23:43 +0200

    Final Update

---
+++ app/public/wp-content/themes/ndlhem/404.php
@@ -0,0 +1,12 @@
+<? get_header() ?>
+
+<div class="bg-blue container" style="margin-top:10%;">
+    <div class="alert alert-secondary text-center">
+        <h2 class="display-4">404</h2>
+        <br>
+        <h5>Oups... Cette page n'existe pas.</h5>
+        <p>Retournez sur le <a href="<? echo home_url() ?>">site</a>.</p>
+    </div>
+</div>
+
+<? get_footer() ?>
